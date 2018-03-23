dc52b0dbd6581c8d2999aa898c5acf29d427c936
Barend Erasmus <developersworkspace@gmail.com>
2018-03-23 08:29:53 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
-import { AES128CTRCryptographyAlgorithm } from './cryptography-algorithms/aes-256-ctr';
+import { AES256CTRCryptographyAlgorithm } from './cryptography-algorithms/aes-256-ctr';
 import { ICryptographyAlgorithm } from './interfaces/cryptography-algorithm';
 import { ITraderAlgorithm } from './interfaces/trader-algorithm';
 import { TraderAlgorithm1 } from './trader-algorithms/trader-algorithm-1';
 import { LunoTrader } from './traders/luno';
 
 (async () => {
-    const cryptographyAlgoritm: ICryptographyAlgorithm = new AES128CTRCryptographyAlgorithm('');
+    const cryptographyAlgoritm: ICryptographyAlgorithm = new AES256CTRCryptographyAlgorithm('');
 
     const keyId: string = 'e2ffd53e85c0075ea3b8d6a98d';
     const keySecret: string = 'dfa5cf66b7d13c548a99b9f9a045d3f9ed9a099b7dd8901305a150a104adf0a206fec78be29ef957a3652c';
